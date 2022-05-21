138dd394c80b53f18c7151f22af031593ae84123
JohnJohnNB <johnjohnptbr@gmail.com>
2022-05-20 21:53:26 -0300

    first commit

---
+++ Imobiliaria.py
@@ -0,0 +1,18 @@
+from Imovel import Imovel
+
+
+class Imobiliaria:
+    def __init__(self):
+        self.__imoveis = []
+
+    @property
+    def imoveis(self):
+        return self.__imoveis
+
+    def incluir_imovel(self, imovel: Imovel):
+        if isinstance(imovel, Imovel) and imovel not in self.__imoveis:
+            self.__imoveis.append(imovel)
+
+    def excluir_imovel(self, imovel: Imovel):
+        if isinstance(imovel, Imovel) and imovel in self.__imoveis:
+            self.__imoveis.remove(imovel)
