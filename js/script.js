105f992616114fd9aac6606ccaeacd02f4e1cd27
GuilhermePrandii <guilhermeprandi@hotmail.com>
2022-10-27 10:19:44 -0200

    Add Logica do AutoClicker

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 let contador = 0
+let autoclicker_count = 0
 
+//Metodo para incrementar o contador geral
 function incrementar_contador(){
     contador += 1
     document.getElementById("contador").textContent = contador + " johns"
 }
 
+// Metodo para Comprar o autoclick
+function BuyAutoClick(){
+    if (contador >= 12){
+        contador -= 12
+        autoclicker_count += 1
+    }
+    Update()
+}
 
+//Metodo para a cada 1 segundo incrementar um cookie
+function Timer(){
+    contador = contador + autoclicker_count
+    Update()
+}
+setInterval(Timer, 1000)
 
+//Metodo para atualizar o contador a cada incremento de cookies ou alguma ação
+function Update(){
+    document.getElementById("contador_autoclick").textContent = "Você tem "+ autoclicker_count + " AutoClickers" 
+    document.getElementById("contador").textContent = contador + " johns"
+}
