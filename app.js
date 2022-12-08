aedf1fb60092aef218fcf2c72af032341544aabb
GuilhermePrandi <101130409+GuilhermePrandii@users.noreply.github.com>
2022-12-07 22:10:33 -0300

    Validação ao cadastrar um user novo

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ const path = require('path')
 const mongoose = require('mongoose')
 require(path.join(__dirname+'/frontend/model/usuario.js'))
 const Usuario = mongoose.model("usuarios")
+const session = require('express-session')
+const flash = require('connect-flash')
 
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: "johnclicker",
+            resave: true,
+            saveUninitialized: true
+        }))
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
@@ -37,21 +45,53 @@ const Usuario = mongoose.model("usuarios")
     app.get('/tela_entrar.html', function(req, res){
         res.sendFile(path.join(__dirname+'/frontend/view/tela_entrar.html'))
     })
+
     app.post('/usuarios/cadastrar', function(req, res){
-        const novoUsuario = {
-            nome: req.body.nome,
-            email: req.body.email,
-            senha: req.body.senha
+        var erros = []
+
+        if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+            erros.push({texto: "Nome inválido"})
         }
 
-        new Usuario(novoUsuario).save().then(() => {
-            console.log('Usuario salvo com sucesso')
-        }).catch((err) =>{
-            console.log('Ocorreu um erro ao salvar o usuario'+ err)
-        })
+        if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+            erros.push({texto: "Email inválido"})
+        }
+
+        if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
+            erros.push({texto: "Senha inválida"})
+        }
+
+        if(req.body.senha.length < 4){
+            erros.push({texto: "Senha muito curta"})
+        }
+
+        if(erros.length > 0){
+            res.render("usuarios/cadastar", {erros: erros})
+            //precisamos fazer com que a tela entrar aponte esses erros ao usuario
+        } else {
+            Usuario.findOne({email: req.body.email}).then((usuario) => {
+                if(usuario){
+                    res.redirect("/tela_entrar.html")
+                    //precisamos tratar aqui pra que retorne uma mensagem para o usuario dizendo que já existe uma conta com esse email
+                } else {
+                    const novoUsuario = new Usuario({
+                        nome: req.body.nome,
+                        email: req.body.email,
+                        senha: req.body.senha 
+                    }).save().then(() => {
+                        console.log("Usúario salvo com sucesso")
+                    }).catch((err) => {
+                        console.log("Ocorreu um erro ao salvar o usúario"+err)
+                    })
+                    res.redirect("/index.html")
+                }
+            }).catch((err) => {
+                res.redirect("/")
+                //mensagem aqui com erro  
+            })
+        }
     })
 
-    //Ele está criando um usúario novo por enquanto ao invés de atualizar o numero de johns daquele usuario
     app.post('/usuarios/salvarjohns', function(req, res){
         const query = {
             //aqi vai o identificador do usuario, pra buscar na query
@@ -84,7 +124,7 @@ const Usuario = mongoose.model("usuarios")
 
 //MongoDB
     mongoose.Promise = global.Promise
-    mongoose.connect("mongodb://localhost/JohnClickerDB", {
+    mongoose.connect("mongodb://127.0.0.1/JohnClickerDB", {
         useNewUrlParser: true
     }).then(() => {
         console.log("MongoDB Conectado!")
--- frontend/model/usuario.js
+++ frontend/model/usuario.js
@@ -15,17 +15,3 @@ const UsuarioSchema = new Schema({
 //Collection (como se fosse uma tabela)
 mongoose.model('usuarios', UsuarioSchema)
 
-//Declarando um objeto dessa tabela
-//const novo_usuario = mongoose.model('usuarios')
-
-/*new novo_usuario({
-    nome: "Teste",
-    num_johns: "404",
-    num_farm: "505"
-}).save().then(() => {
-    console.log("Salvo com sucesso")
-    console.log("Usuário criado com sucesso")
-}).catch(() => {
-    console.log("Houve um erro ao registrar o úsuario")
-})
-*/
